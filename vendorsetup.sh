1334f0224942ace5b5f871c9f02af6f7d9ff9b4b
JoseXda <josegustavo6@protonmail.com>
2024-01-17 19:34:19 -0300

    update vendorsetup
    .

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,31 +1,14 @@
-#!/bin/bash
-
-echo 'Starting Cloning repos for rhode'
-echo 'Cloning Kernel tree [1/7]'
-
 rm -rf kernel/motorola/sm6225
-git clone https://github.com/SM6225-Project/kernel_motorola_sm6225 kernel/motorola/sm6225
-
-echo 'Cloning Common Tree [2/7]'
+git clone --depth=1 https://github.com/SM6225-Project/kernel_motorola_sm6225 kernel/motorola/sm6225
 
 rm -rf device/motorola/sm6225-common
-git clone https://github.com/SM6225-Project/device_motorola_sm6225-common device/motorola/sm6225-common
+git clone --depth=1 https://github.com/ProjectElixir-Devices/device_motorola_sm6225-common -b UNO device/motorola/sm6225-common
 
-echo 'Cloning Device Tree [3/7]'
-
-rm -rf device/motorola/hawao
-git clone https://github.com/ProjectElixir-Devices/android_device_motorola_hawao device/motorola/hawao
-
-echo 'Cloning Vendor Trees [4/7]'
-
-rm -rf vendor/motorola/sm6225-common
-git clone https://github.com/JoseXda/vendor_motorola_sm6225-common vendor/motorola/sm6225-common
-rm -rf vendor/motorola/hawao
-git clone https://github.com/TheMuppets/proprietary_vendor_motorola_hawao vendor/motorola/hawao
-
-echo 'Cloning Motorola´s common stuffs [5/7]'
+rm -rf vendor/motorola
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_motorola_sm6225-common -b UNO vendor/motorola/sm6225-common
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_motorola_hawao -b UNO vendor/motorola/hawao
 
 rm -rf hardware/motorola
-git clone https://github.com/Project-Elixir/hardware_motorola hardware/motorola
-rm -rf system/qcom
-git clone https://github.com/Project-Elixir/system_qcom system/qcom
+git clone --depth=1 https://github.com/LineageOS/android_hardware_motorola -b lineage-21 hardware/motorola
+
+rm -rf hardware/qcom-caf/bengal/gps
